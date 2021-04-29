f818ebb8084bc10287afc4d56954f6637cf6fbc9
andresmolanoe <af.molano@uniandes.edu.co>
2021-04-29 12:19:45 -0500

    punto 1

--- scr/task-2.R
+++ scr/task-2.R
@@ -1,1 +1,14 @@
-#Task-2 Taller de R
+#Elaborado por: Joaquin Correa (201730895), Andres Molano (201814276), Ricardo Rodriguez(201822271)
+#Fecha: 29 de abril de 2021
+
+#Taller A
+
+#Eliminar objetos antiguos
+rm(list=ls())
+
+#Cargar y/o instalar paquetes
+pacman::p_load(tidyverse,reshape2,readxl,data.table)
+
+#1. Loops (40% nota)
+  #1.0 
+  lista_df = readRDS("data/input/lista.rds")

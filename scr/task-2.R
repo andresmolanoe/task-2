8d9af079e378a43b98e6b54cdf7556489979d8c2
Ricisco <78061519+Ricisco@users.noreply.github.com>
2021-05-02 20:51:36 -0500

    Update task-2.R

--- scr/task-2.R
+++ scr/task-2.R
@@ -3,6 +3,7 @@
 
 #Taller A
 
+
 #Eliminar objetos antiguos
 rm(list=ls())
 
@@ -12,7 +13,34 @@ pacman::p_load(tidyverse,reshape2,readxl,data.table)
 #1. Loops (40% nota)
   #1.0 
   lista_df <- readRDS("data/input/lista.rds")
-  
-  #1.1 
+  # Para observar los nombres de los diferentes data frames
+  lapply(lista_df, function (x) colnames (x))
+  names(lista_df)
+         
+  #1.1 Eliminar todos los NA y renombrar la primera fila de variables
+  for (i in 1:lenght(lista_df)){
+       lista_df[[i]] = lista_df[[i]] %>% subset(is.na(...2)==F)
+       colnames(lista_df[[i]]) = lista_df[[i]][1,] %>% unlist() %>% as.character() %>% tolower() %>% chartr"áéíóú","aeiou",lista_df[[i]][1,]
+       lista_df[[i]] = lista_df[[i]][-1,] %>% data.frame(stringsAsFactors = F)
+    }
+    
+  #1.2 Agregar una variable que indica el tipo de delito
+  for (i in 1:lenght(lista_df)){
+       lista_df[[i]] = lista_df[[i]] %>% mutate(type_delito = names(lista_df[[i]]))
+    }
+                                  
+  #1.3 Apilar los datos
+  df = data.table::rbindlist(l = lista_df. use.names = T, fill = T)
+         
+ #2. Familia apply (30% nota)
+   #2.1 Pintamos sobre la consola una tabla de frecuencia para cada variable
+   lapply(df, function(x) table (x) %>% sort(decreasing = T) %>% head(10))
+         
+ #3. Familia apply (30% nota)
+   #3.1 Creamos la funcion y para que todas los elementos de un vector de caracteres queden escritos en minuscula
+   y <- 
+          
+   #3.2 Aplicamos esta funcion a df
+         
   
   
